7d589a4251c436a813353e69d0214e33d0b3aeb5
zhaoyang110157 <1853935753@qq.com>
2019-04-20 22:03:54 +0800

    4.20 complete User Permission Change Function

--- src/store/modules/Person.js
+++ src/store/modules/Person.js
@@ -12,6 +12,20 @@ const actions = {
             .then((res)=>{
                 context.commit('getUsers',res);
             })
+    },
+    changeAllow(context,index){
+        console.log(index);
+
+        let data = new FormData();
+        data.append("account",state.users[index].account)
+        let allowed = (state.users[index].allowed)? 0:1; console.log(state.users[index].account + "\n"+allowed)
+        data.append("allowed",allowed)
+        Axios.post('http://localhost:8000/serv_war_exploded/Users',data)
+            .then((res)=>{
+                context.commit('changeAllow',index)
+                console.log("message");
+            });
+
     }
 }
 const mutations = {
@@ -30,6 +44,7 @@ const mutations = {
     },
     changeAllow (state, index) {
         state.users[index].allowed = !state.users[index].allowed;
+        console.log(state.users[index].account)
     }
 };
 
